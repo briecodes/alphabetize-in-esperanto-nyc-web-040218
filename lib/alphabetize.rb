e7ce1951c8d7729f8e8538b34c9af32a287f12fa
briecodes <hello@brie.work>
2018-03-21 20:16:31 -0400

    Esperanto Alphabetizing

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
 def alphabetize(arr)
-  # code here
+  alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".each_char.with_index.to_h
+
+  arr.sort_by do |words|
+    words.chars.map { |c|
+      alpha[c]
+    }
+  end
+
 end
